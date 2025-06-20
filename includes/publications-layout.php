1f8d263b5814df08ddd64db4f224c74cd294a1e2
Kat-Gumerov <katrinagumerov24@gmail.com>
2025-06-20 18:51:22 +0000

    changing null values

--- includes/publications-layout.php
+++ includes/publications-layout.php
@@ -92,7 +92,6 @@
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-2 form-group">
                         <select name="pubAuth[]" id="pubAuth" class="form-control" style="width: 100%;" aria-label="Filter publications by author" multiple>
-							<option value="0">Author</option>
 							<?php for ( $i = 0; $i < count( $pubAuth_arr ); $i++ ) : ?>
 								<option value="<?= $pubAuth_arr[ $i ]->PeopleID ?>">
 									<?= $pubAuth_arr[ $i ]->LastFirstName ?>
